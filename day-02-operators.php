d555f5a42708911045e01d35f66558a9912ed320
Samia Mahi <mahisamia13@gmail.com>
2019-07-08 18:34:34 +0100

    Create day-02-operators.php

---
+++ day-02-operators.php
@@ -0,0 +1,24 @@
+<?php
+// Complete the solve function below.
+function solve($meal_cost, $tip_percent, $tax_percent) {
+    $tip = $meal_cost * ($tip_percent/100);
+    $tax = $meal_cost * ($tax_percent/100);
+
+    $total = round($meal_cost + $tip + $tax);
+    print($total); 
+
+}
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%lf\n", $meal_cost);
+
+fscanf($stdin, "%d\n", $tip_percent);
+
+fscanf($stdin, "%d\n", $tax_percent);
+
+solve($meal_cost, $tip_percent, $tax_percent);
+
+fclose($stdin);
+
+?>
